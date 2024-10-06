d903a322bfe5cf08fb51f7f6741d267bc325c636
Sagar47864 <159270473+Sagar47864@users.noreply.github.com>
2024-10-06 20:39:32 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// This script can be enhanced to filter content based on user input
+document.querySelector('.search-icon').addEventListener('click', function() {
+    alert('Search functionality coming soon!');
+  });
+  
